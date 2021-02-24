d135c39176179beeadade928d40e549f0f557da9
Avishek Das <davishek7@gmail.com>
2021-02-24 12:14:25 +0530

    staticfiles dir added

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    '/var/www/static/',
+    BASE_DIR / 'static'
 ]
 
 STATIC_ROOT=(BASE_DIR/'staticfiles')
